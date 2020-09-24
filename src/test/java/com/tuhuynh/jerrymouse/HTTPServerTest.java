e8410a59342af24ffa8eea39b15ca6b601e4d7b1
Tu Huynh <tu.huynh@linecorp.com>
2020-09-24 13:18:02 +0700

    abstract unit tests

--- src/test/java/com/tuhuynh/jerrymouse/HTTPServerTest.java
+++ src/test/java/com/tuhuynh/jerrymouse/HTTPServerTest.java
@@ -6,22 +6,14 @@ import lombok.val;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @DisplayName("api.HttpServerTest")
-public class HTTPServerTest {
-    final int port = 1234;
-    final String url = "http://localhost:" + port;
-
-    @BeforeEach
-    void each() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(1);
+public class HTTPServerTest extends HTTPTest {
+    public HTTPServerTest() {
+        this.url = "http://localhost:1234";
     }
 
     @BeforeAll
@@ -85,125 +77,8 @@ public class HTTPServerTest {
         TimeUnit.SECONDS.sleep(3);
     }
 
-    @Test
-    @DisplayName("Hello World test")
-    void helloWorld() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/").method("GET")
-                .build().perform();
-        assertEquals(res.getBody(), "Hello World");
-    }
-
-    @Test
-    @DisplayName("Echo test")
-    void echo() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/echo").method("POST").body("copycat")
-                .build().perform();
-        assertEquals(res.getBody(), "copycat");
-    }
-
-    @Test
-    @DisplayName("Query Params test")
-    void queryParams() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/query?hello=world").method("GET")
-                .build().perform();
-        assertEquals(res.getBody(), "world");
-    }
-
-    @Test
-    @DisplayName("Path Params test")
-    void pathParams() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/path/hello/123").method("GET")
-                .build().perform();
-        assertEquals(res.getBody(), "hello:123");
-    }
-
-    @Test
-    @DisplayName("Catch All test")
-    void catchAll() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/all/123/456").method("GET")
-                .build().perform();
-        assertEquals(res.getBody(), "/all/123/456");
-    }
-
-    @Test
-    @DisplayName("Middleware test failed")
-    void middlewareFail() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/protected").method("GET")
-                .build().perform();
-        assertEquals(res.getStatus(), 401);
-        assertEquals(res.getBody(), "invalid_token");
-    }
-
-    @Test
-    @DisplayName("Middleware test success")
-    void middlewareSuccess() throws IOException {
-        val headers = new HashMap<String, String>();
-        headers.put("Authorization", "Bearer test_token");
-        val res = HttpClient.builder()
-                .url(url + "/protected").method("GET").headers(headers)
-                .build().perform();
-        assertEquals(res.getStatus(), 200);
-        assertEquals(res.getBody(), "success:tuhuynh");
-    }
-
-    @Test
-    @DisplayName("Panic")
-    void panic() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/panic").method("GET")
-                .build().perform();
-        assertEquals(res.getStatus(), 500);
-        assertEquals(res.getBody(), "Panicked!");
-    }
-
-    @Test
-    @DisplayName("Global Middleware")
-    void globalMiddleware() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/gm").method("GET")
-                .build().perform();
-        assertEquals(res.getBody(), "middleware");
-    }
-
-    @Test
-    @DisplayName("Global Middleware not from subRouter")
-    void globalMiddlewareNotFromSubRouter() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/gm").method("GET")
-                .build().perform();
-        assertEquals(res.getBody(), "middleware");
-    }
-
-    @Test
-    @DisplayName("SubRouter1")
-    void subRouter1() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/cat").method("GET")
-                .build().perform();
-        assertEquals(res.getBody(), "this is a cat");
-    }
-
-    @Test
-    @DisplayName("SubRouter2")
-    void subRouter2() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/cat/echo").method("POST").body("copycat")
-                .build().perform();
-        assertEquals(res.getBody(), "copycat");
-    }
-
-    @Test
-    @DisplayName("SubRouter3")
-    void subRouter3() throws IOException {
-        val res = HttpClient.builder()
-                .url(url + "/cat/gm").method("GET")
-                .build().perform();
-        assertEquals(res.getBody(), "cat");
+    @BeforeEach
+    void each() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(1);
     }
 }
---
+++ src/test/java/com/tuhuynh/jerrymouse/HTTPTest.java
@@ -0,0 +1,136 @@
+package com.tuhuynh.jerrymouse;
+
+import lombok.val;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public abstract class HTTPTest {
+    protected String url = "";
+
+    @Test
+    @DisplayName("Hello World test")
+    void helloWorld() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/").method("GET")
+                .build().perform();
+        assertEquals(res.getBody(), "Hello World");
+    }
+
+    @Test
+    @DisplayName("Echo test")
+    void echo() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/echo").method("POST").body("copycat")
+                .build().perform();
+        assertEquals(res.getBody(), "copycat");
+    }
+
+    @Test
+    @DisplayName("Query Params test")
+    void queryParams() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/query?hello=world").method("GET")
+                .build().perform();
+        assertEquals(res.getBody(), "world");
+    }
+
+    @Test
+    @DisplayName("Path Params test")
+    void pathParams() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/path/hello/123").method("GET")
+                .build().perform();
+        assertEquals(res.getBody(), "hello:123");
+    }
+
+    @Test
+    @DisplayName("Catch All test")
+    void catchAll() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/all/123/456").method("GET")
+                .build().perform();
+        assertEquals(res.getBody(), "/all/123/456");
+    }
+
+    @Test
+    @DisplayName("Middleware test failed")
+    void middlewareFail() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/protected").method("GET")
+                .build().perform();
+        assertEquals(res.getStatus(), 401);
+        assertEquals(res.getBody(), "invalid_token");
+    }
+
+    @Test
+    @DisplayName("Middleware test success")
+    void middlewareSuccess() throws IOException {
+        val headers = new HashMap<String, String>();
+        headers.put("Authorization", "Bearer test_token");
+        val res = HttpClient.builder()
+                .url(url + "/protected").method("GET").headers(headers)
+                .build().perform();
+        assertEquals(res.getStatus(), 200);
+        assertEquals(res.getBody(), "success:tuhuynh");
+    }
+
+    @Test
+    @DisplayName("Panic")
+    void panic() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/panic").method("GET")
+                .build().perform();
+        assertEquals(res.getStatus(), 500);
+        assertEquals(res.getBody(), "Panicked!");
+    }
+
+    @Test
+    @DisplayName("Global Middleware")
+    void globalMiddleware() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/gm").method("GET")
+                .build().perform();
+        assertEquals(res.getBody(), "middleware");
+    }
+
+    @Test
+    @DisplayName("Global Middleware not from subRouter")
+    void globalMiddlewareNotFromSubRouter() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/gm").method("GET")
+                .build().perform();
+        assertEquals(res.getBody(), "middleware");
+    }
+
+    @Test
+    @DisplayName("SubRouter1")
+    void subRouter1() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/cat").method("GET")
+                .build().perform();
+        assertEquals(res.getBody(), "this is a cat");
+    }
+
+    @Test
+    @DisplayName("SubRouter2")
+    void subRouter2() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/cat/echo").method("POST").body("copycat")
+                .build().perform();
+        assertEquals(res.getBody(), "copycat");
+    }
+
+    @Test
+    @DisplayName("SubRouter3")
+    void subRouter3() throws IOException {
+        val res = HttpClient.builder()
+                .url(url + "/cat/gm").method("GET")
+                .build().perform();
+        assertEquals(res.getBody(), "cat");
+    }
+}
