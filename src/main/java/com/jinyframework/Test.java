7d085a7f3f1236d66edf0f27f73bcfd86fb49a95
Tu Huynh <tu.huynh@linecorp.com>
2020-10-07 02:12:04 +0700

    harharhar

--- src/main/java/com/jinyframework/Test.java
+++ src/main/java/com/jinyframework/Test.java
@@ -1,22 +1,14 @@
 package com.jinyframework;
 
-import com.jinyframework.core.RequestBinderBase.HttpResponse;
 import lombok.val;
 
 import java.io.IOException;
 
 public final class Test {
     public static void main(String[] args) throws IOException {
-        val server = NIOHttpServer.port(1234);
-
-        // CORS middleware
-        server.use(ctx -> {
-            ctx.setResponseHeader("Access-Control-Allow-Origin", "*");
-            return HttpResponse.nextAsync();
-        });
-
-        server.get("/", ctx -> HttpResponse.ofAsync("Hello World!"));
-
-        server.start();
+        val proxy = NIOHttpProxy.port(1234);
+        proxy.use("/server1", "localhost:1111");
+        proxy.use("/server2", "linecorp.com:80");
+        proxy.start();
     }
 }
