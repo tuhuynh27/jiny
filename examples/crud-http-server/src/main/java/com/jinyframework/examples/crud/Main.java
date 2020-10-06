8832e8a5b00e4f1c2ee1bba60a2bbf07408563f3
Tu Huynh <tu.huynh@linecorp.com>
2020-10-06 17:18:46 +0700

    update crud example

--- examples/crud-http-server/src/main/java/com/jinyframework/examples/crud/Main.java
+++ examples/crud-http-server/src/main/java/com/jinyframework/examples/crud/Main.java
@@ -1,5 +1,6 @@
 package com.jinyframework.examples.crud;
 
+import com.google.gson.Gson;
 import com.jinyframework.examples.crud.router.CatRouter;
 import com.jinyframework.HttpServer;
 import com.jinyframework.core.RequestBinderBase.HttpResponse;
@@ -10,9 +11,12 @@ import java.io.IOException;
 public final class Main {
     public static void main(String[] args) throws IOException {
         val server = HttpServer.port(1234);
+        server.useTransformer(res -> new Gson().toJson(res));
+
         server.get("/", ctx -> HttpResponse.of("CRUD"));
         val catRouter = CatRouter.getRouter();
         server.use("/cat", catRouter);
+
         server.start();
     }
 }
--- examples/crud-http-server/src/main/java/com/jinyframework/examples/crud/utils/ResponseHelper.java
+++ examples/crud-http-server/src/main/java/com/jinyframework/examples/crud/utils/ResponseHelper.java
@@ -1,20 +1,17 @@
 package com.jinyframework.examples.crud.utils;
 
-import com.google.gson.Gson;
-import com.jinyframework.examples.crud.entities.ResponseEntity;
 import com.jinyframework.core.RequestBinderBase.HttpResponse;
+import com.jinyframework.examples.crud.entities.ResponseEntity;
 import lombok.val;
 
 public final class ResponseHelper {
     public static HttpResponse success(String msg) {
-        val gson = new Gson();
         val obj = ResponseEntity.builder().message(msg).build();
-        return HttpResponse.of(obj).transform(gson::toJson);
+        return HttpResponse.of(obj);
     }
 
     public static HttpResponse error(String msg) {
-        val gson = new Gson();
         val obj = ResponseEntity.builder().error(msg).build();
-        return HttpResponse.of(obj).transform(gson::toJson).status(400);
+        return HttpResponse.of(obj);
     }
 }
