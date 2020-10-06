048c5213e214a6ebf73e269dfbc1b2a8e8d9e264
Tu Huynh <tu.huynh@linecorp.com>
2020-10-06 18:05:16 +0700

    update scala-crud example

--- examples/scala-crud-http-server/src/main/scala/com/jinyframework/examples/scalacrud/Main.scala
+++ examples/scala-crud-http-server/src/main/scala/com/jinyframework/examples/scalacrud/Main.scala
@@ -1,12 +1,12 @@
 package com.jinyframework.examples.scalacrud
 
-import com.jinyframework.Proxy
+import com.jinyframework.HttpProxy
 
 object Main extends App {
   new Thread(ServerBootstrap).start() // Port 1234
   new Thread(ServerBootstrapNIO).start() // Port 1235
 
-  val proxy = Proxy.port(1111)
+  val proxy = HttpProxy.port(1111)
   proxy.use("/server1", "localhost:1234")
   proxy.use("/server2", "localhost:1235")
   proxy.start()
