2ee06914c8cf7c3b3bf2ef60f3063c44107640d2
Tu Huynh <tu.huynh@linecorp.com>
2020-10-23 17:07:05 +0700

    update sample middleware

--- middlewares/src/main/java/com/jinyframework/Middlewares.java
+++ middlewares/src/main/java/com/jinyframework/Middlewares.java
@@ -1,7 +1,14 @@
 package com.jinyframework;
 
+import com.jinyframework.core.AbstractRequestBinder.Context;
+import com.jinyframework.core.utils.ParserUtils.HttpMethod;
+import lombok.val;
+
 public final class Middlewares {
     public static String test() {
-        return "This is middleware method";
+        // TODO: Sample code, need to be updated when done CORS middleware
+        val context = Context.builder()
+                .path("/").method(HttpMethod.GET).build();
+        return context.getBody();
     }
 }
