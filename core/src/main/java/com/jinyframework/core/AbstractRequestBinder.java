caa2b2aa7cd31bb45f3560e85ad77a569632fa2d
Viet (Blu) <nguyenqviet98@gmail.com>
2020-11-12 13:32:10 +0700

    barely working impl

--- core/src/main/java/com/jinyframework/core/AbstractRequestBinder.java
+++ core/src/main/java/com/jinyframework/core/AbstractRequestBinder.java
@@ -170,6 +170,10 @@ public abstract class AbstractRequestBinder<T extends HandlerBase> {
             return new HttpResponse(400, errorText, false);
         }
 
+        public static HttpResponse reject(final String errorText,final int httpStatusCode) {
+            return new HttpResponse(httpStatusCode, errorText, false);
+        }
+
         public static CompletableFuture<HttpResponse> rejectAsync(final String errorText) {
             return CompletableFuture.completedFuture(new HttpResponse(400, errorText, false));
         }
--- middlewares/src/main/java/com/jinyframework/middlewares/jwt/Jwt.java
+++ middlewares/src/main/java/com/jinyframework/middlewares/jwt/Jwt.java
@@ -1,19 +1,20 @@
 package com.jinyframework.middlewares.jwt;
 
-import com.google.gson.Gson;
 import com.jinyframework.core.AbstractRequestBinder;
 import com.jinyframework.core.AbstractRequestBinder.Handler;
 import com.jinyframework.core.AbstractRequestBinder.HttpResponse;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NonNull;
+import lombok.*;
 import lombok.experimental.Accessors;
-import lombok.val;
 
+import javax.crypto.SecretKey;
 import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public final class Jwt {
@@ -27,21 +28,41 @@ public final class Jwt {
     public static class AuthComponent {
         private final Handler handlePermission;
         private final Handler handleRefresh;
+        /**
+         * Handler that extracts user authentication info, authenticates user, then response with a JWT
+         */
         private final Handler handleLogin;
+
+        /**
+         * Handler that extracts JWT, verifies it, retrieve user info with claims
+         * then populate context with user info.
+         */
         private final Handler handleVerify;
     }
 
-    public static AuthComponent newAuthComponent(@NonNull Config config) throws Exception {
+    public static String genHS256Key() {
+        return Arrays.toString(Keys.secretKeyFor(SignatureAlgorithm.HS256).getEncoded());
+    }
+
+    @SneakyThrows
+    public static AuthComponent newAuthComponent(@NonNull Config config) {
         if (config.secretKey == null) {
             throw new Exception("Secret key not provided");
         }
         val builder = config.toBuilder();
 
+        if (config.userKey == null) {
+            builder.userKey(Config.userKeyDefault);
+        }
         if (config.userRetriever == null) {
-            final UserRetriever<String> retriever = (ctx, claims) -> String.valueOf(claims.get(Claims.SUBJECT));
+            final UserRetriever retriever = (ctx, claims) -> String.valueOf(claims.get(Claims.SUBJECT));
             builder.userRetriever(retriever);
         }
 
+        if (config.authenticator == null) {
+            final Authenticator authenticator = ctx -> new HashMap<>();
+            builder.authenticator(authenticator);
+        }
         val finalConfig = builder.build();
 
         return AuthComponent.builder()
@@ -55,10 +76,10 @@ public final class Jwt {
 
     private static String extractToken(AbstractRequestBinder.Context ctx) {
         val header = ctx.headerParam(authHeaderKey);
-        return header.startsWith(authHeaderPrefix) ? header.substring(authHeaderPrefix.length()) : header;
+        return header.startsWith(authHeaderPrefix) ? header.substring(authHeaderPrefix.length()) : null;
     }
 
-    private static Map<String, Object> verifyToken(String key, String token) {
+    private static Map<String, Object> verifyToken(SecretKey key, String token) {
         return Jwts.parserBuilder().setSigningKey(key)
                 .build()
                 .parseClaimsJws(token).getBody();
@@ -67,7 +88,16 @@ public final class Jwt {
     private static Handler verifyHandler(@NonNull Config config) {
         return ctx -> {
             val tokStr = extractToken(ctx);
-            val claims = verifyToken(config.secretKey, tokStr);
+            if (tokStr == null) {
+                return HttpResponse.reject("Authentication failed", HttpURLConnection.HTTP_UNAUTHORIZED);
+            }
+            val secretKey = Keys.hmacShaKeyFor(config.secretKey.getBytes(StandardCharsets.UTF_8));
+            final Map<String, Object> claims;
+            try {
+                claims = verifyToken(secretKey, tokStr);
+            } catch (Exception e) {
+                return HttpResponse.reject(e.toString(), HttpURLConnection.HTTP_UNAUTHORIZED);
+            }
             val user = config.userRetriever.retrieveUser(ctx, claims);
             ctx.setDataParam(config.userKey, user);
             return HttpResponse.next();
@@ -75,36 +105,30 @@ public final class Jwt {
     }
 
     @FunctionalInterface
-    public interface UserRetriever<T> {
-        T retrieveUser(AbstractRequestBinder.Context ctx, Map<String, Object> claims);
+    public interface UserRetriever {
+        Object retrieveUser(AbstractRequestBinder.Context ctx, Map<String, Object> claims);
     }
 
-    @Getter
-    public static class UserAuthReq {
-        String username;
-        String password;
-    }
-
-    private static UserAuthReq extractUserAuth(AbstractRequestBinder.Context ctx) {
-        return new Gson().fromJson(ctx.getBody(), UserAuthReq.class);
-    }
-
-    private static boolean validUser(UserAuthReq userAuthReq) {
-        return true;
+    @FunctionalInterface
+    public interface Authenticator {
+        Map<String, Object> authenticate(AbstractRequestBinder.Context ctx) throws Exception;
     }
 
     private static Handler loginHandler(@NonNull Config config) {
         return ctx -> {
-            val userAuthReq = extractUserAuth(ctx);
-            if (!validUser(userAuthReq)) {
-                return HttpResponse.of("Wrong username or password", HttpURLConnection.HTTP_UNAUTHORIZED);
+            val claims = config.authenticator.authenticate(ctx);
+            if (claims == null) {
+                return HttpResponse.reject("Authentication failed", HttpURLConnection.HTTP_UNAUTHORIZED);
             }
-            val secretKey = Keys.hmacShaKeyFor(config.secretKey.getBytes());
-            Jwts.builder()
-                    .setSubject("")
+            val secretKey = Keys.hmacShaKeyFor(config.secretKey.getBytes(StandardCharsets.UTF_8));
+            val token = Jwts.builder()
+                    .setSubject("admin")
+                    .addClaims(claims)
                     .signWith(secretKey)
                     .compact();
-            return HttpResponse.next();
+            val authHeaderVal = authHeaderPrefix + token;
+            ctx.getResponseHeaders().put(authHeaderKey, authHeaderVal);
+            return HttpResponse.of("Authentication successful");
         };
     }
 
@@ -116,8 +140,10 @@ public final class Jwt {
          */
         private final String secretKey;
 
+        public static final String userKeyDefault = "authUser";
         /**
          * Key used for getting data from context
+         * Default: authUser
          */
         private final String userKey;
 
@@ -126,7 +152,15 @@ public final class Jwt {
          * This data will be populated to context.
          * Default: returns {@code claims.get("sub")}
          */
-        @SuppressWarnings("rawtypes")
         private final UserRetriever userRetriever;
+
+
+        /**
+         * Function that authenticates user given the Context object
+         * and returns claims map to add to the JWT.
+         * This will be used by the login handler. It should return null if authentication fails
+         * Default: returns an empty HashMap
+         */
+        private final Authenticator authenticator;
     }
 }
