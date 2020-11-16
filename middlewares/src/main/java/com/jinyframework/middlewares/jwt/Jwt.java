e907b47d385d86b26da6f2c3cc4ec3264be3c432
Viet (Blu) <nguyenqviet98@gmail.com>
2020-11-16 18:09:41 +0700

    refactor

--- middlewares/src/main/java/com/jinyframework/middlewares/jwt/Jwt.java
+++ middlewares/src/main/java/com/jinyframework/middlewares/jwt/Jwt.java
@@ -3,7 +3,6 @@ package com.jinyframework.middlewares.jwt;
 import com.jinyframework.core.AbstractRequestBinder;
 import com.jinyframework.core.AbstractRequestBinder.Handler;
 import com.jinyframework.core.AbstractRequestBinder.HttpResponse;
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -19,25 +18,10 @@ import java.util.Map;
 
 public final class Jwt {
 
+    private static final String authHeaderKey = "Authorization";
+    private static final String authHeaderPrefix = "Bearer ";
+
     private Jwt() {
-    }
-
-    @Accessors(fluent = true)
-    @Getter
-    @Builder(toBuilder = true)
-    public static class AuthComponent {
-        private final Handler handlePermission;
-        private final Handler handleRefresh;
-        /**
-         * Handler that extracts user authentication info, authenticates user, then response with a JWT
-         */
-        private final Handler handleLogin;
-
-        /**
-         * Handler that extracts JWT, verifies it, retrieve user info with claims
-         * then populate context with user info.
-         */
-        private final Handler handleVerify;
     }
 
     public static String genHS256Key() {
@@ -50,6 +34,9 @@ public final class Jwt {
             throw new Exception("Secret key not provided");
         }
         val builder = config.toBuilder();
+
+        val secKeyObj = Keys.hmacShaKeyFor(config.secretKey.getBytes(StandardCharsets.UTF_8));
+        builder.secKeyObj(secKeyObj);
 
         if (config.userKey == null) {
             builder.userKey(Config.userKeyDefault);
@@ -71,9 +58,6 @@ public final class Jwt {
                 .build();
     }
 
-    private static final String authHeaderKey = "Authorization";
-    private static final String authHeaderPrefix = "Bearer ";
-
     private static String extractToken(AbstractRequestBinder.Context ctx) {
         val header = ctx.headerParam(authHeaderKey);
         return header.startsWith(authHeaderPrefix) ? header.substring(authHeaderPrefix.length()) : null;
@@ -91,16 +75,31 @@ public final class Jwt {
             if (tokStr == null) {
                 return HttpResponse.reject("Authentication failed", HttpURLConnection.HTTP_UNAUTHORIZED);
             }
-            val secretKey = Keys.hmacShaKeyFor(config.secretKey.getBytes(StandardCharsets.UTF_8));
             final Map<String, Object> claims;
             try {
-                claims = verifyToken(secretKey, tokStr);
+                claims = verifyToken(config.secKeyObj, tokStr);
             } catch (Exception e) {
                 return HttpResponse.reject(e.toString(), HttpURLConnection.HTTP_UNAUTHORIZED);
             }
             val user = config.userRetriever.retrieveUser(ctx, claims);
             ctx.setDataParam(config.userKey, user);
             return HttpResponse.next();
+        };
+    }
+
+    private static Handler loginHandler(@NonNull Config config) {
+        return ctx -> {
+            val claims = config.authenticator.authenticate(ctx);
+            if (claims == null) {
+                return HttpResponse.reject("Authentication failed", HttpURLConnection.HTTP_UNAUTHORIZED);
+            }
+            val token = Jwts.builder()
+                    .addClaims(claims)
+                    .signWith(config.secKeyObj)
+                    .compact();
+            val authHeaderVal = authHeaderPrefix + token;
+            ctx.getResponseHeaders().put(authHeaderKey, authHeaderVal);
+            return HttpResponse.of("Authentication successful");
         };
     }
 
@@ -114,33 +113,38 @@ public final class Jwt {
         Map<String, Object> authenticate(AbstractRequestBinder.Context ctx) throws Exception;
     }
 
-    private static Handler loginHandler(@NonNull Config config) {
-        return ctx -> {
-            val claims = config.authenticator.authenticate(ctx);
-            if (claims == null) {
-                return HttpResponse.reject("Authentication failed", HttpURLConnection.HTTP_UNAUTHORIZED);
-            }
-            val secretKey = Keys.hmacShaKeyFor(config.secretKey.getBytes(StandardCharsets.UTF_8));
-            val token = Jwts.builder()
-                    .setSubject("admin")
-                    .addClaims(claims)
-                    .signWith(secretKey)
-                    .compact();
-            val authHeaderVal = authHeaderPrefix + token;
-            ctx.getResponseHeaders().put(authHeaderKey, authHeaderVal);
-            return HttpResponse.of("Authentication successful");
-        };
+    @SuppressWarnings("ClassNameSameAsAncestorName")
+    public interface Claims extends io.jsonwebtoken.Claims {
+
+    }
+
+    @Accessors(fluent = true)
+    @Getter
+    @Builder(toBuilder = true)
+    public static class AuthComponent {
+        private final Handler handlePermission;
+        private final Handler handleRefresh;
+        /**
+         * Handler that extracts user authentication info, authenticates user, then response with a JWT
+         */
+        private final Handler handleLogin;
+
+        /**
+         * Handler that extracts JWT, verifies it, retrieve user info with claims
+         * then populate context with user info.
+         */
+        private final Handler handleVerify;
     }
 
     @Getter
     @Builder(toBuilder = true)
     public static final class Config {
+        public static final String userKeyDefault = "authUser";
         /**
-         * Secret key used for signing. Required.
+         * Secret key used for signing. Required and must be long enough. Recommend using {@link #genHS256Key()}
          */
         private final String secretKey;
-
-        public static final String userKeyDefault = "authUser";
+        private final SecretKey secKeyObj;
         /**
          * Key used for getting data from context
          * Default: authUser
@@ -154,7 +158,6 @@ public final class Jwt {
          */
         private final UserRetriever userRetriever;
 
-
         /**
          * Function that authenticates user given the Context object
          * and returns claims map to add to the JWT.
@@ -162,5 +165,11 @@ public final class Jwt {
          * Default: returns an empty HashMap
          */
         private final Authenticator authenticator;
+
+        public static class ConfigBuilder {
+            private ConfigBuilder secKeyObj(SecretKey secKeyObj) {
+                return this;
+            }
+        }
     }
 }
