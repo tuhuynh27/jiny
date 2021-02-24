f14424405692c457772a40011b7d806440c11e36
Tu Huynh <tu.huynh@linecorp.com>
2021-02-24 11:21:54 +0700

    update override

--- keva/server/src/main/java/com/jinyframework/keva/server/storage/KevaStoreImpl.java
+++ keva/server/src/main/java/com/jinyframework/keva/server/storage/KevaStoreImpl.java
@@ -7,22 +7,27 @@ public class KevaStoreImpl implements KevaStore {
     private final ConcurrentHashMap<String, Object> keva = new ConcurrentHashMap<>();
     private final Timer timer = new Timer();
 
+    @Override
     public Object get(String key) {
         return keva.get(key);
     }
 
+    @Override
     public void put(String key, Object value) {
         keva.put(key, value);
     }
 
+    @Override
     public void putAll(Map<String, Object> m) {
         keva.putAll(m);
     }
 
+    @Override
     public void remove(String key) {
         keva.remove(key);
     }
 
+    @Override
     public void expire(String key, long expireTimeInMilliSecond) {
         timer.schedule(new TimerTask() {
             @Override
@@ -32,6 +37,7 @@ public class KevaStoreImpl implements KevaStore {
         }, expireTimeInMilliSecond);
     }
 
+    @Override
     public Set<Map.Entry<String, Object>> entrySetCopy() {
         return new HashMap<>(keva).entrySet();
     }
