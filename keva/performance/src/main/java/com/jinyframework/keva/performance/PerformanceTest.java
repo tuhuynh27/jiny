4cdf963546b44faa7b94ae5593998c53d25bc741
Tu Huynh <tu.huynh@linecorp.com>
2021-03-08 17:06:38 +0700

    update

--- keva/performance/src/main/java/com/jinyframework/keva/performance/PerformanceTest.java
+++ keva/performance/src/main/java/com/jinyframework/keva/performance/PerformanceTest.java
@@ -7,48 +7,58 @@ import lombok.val;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class PerformanceTest {
-    public final static int THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors();
+    public final static int THREAD_POOL_SIZE = 1;
 
     public static void main(String[] args) throws InterruptedException {
         val manager = new NoHeapStoreManager();
         manager.createStore("Performance-Test", NoHeapStore.Storage.IN_MEMORY, 1843);
         val noHeapStore = manager.getStore("Performance-Test");
-        performTest(noHeapStore);
-    }
-
-    // Currently single thread test
-    public static void performTest(NoHeapStore noHeapStore) throws InterruptedException {
-        log.info("Thread usage: " + THREAD_POOL_SIZE);
-
         ArrayList<String> list = new ArrayList<>();
         for (int i1 = 1; i1 < 1_000_000; i1++) {
             list.add(String.valueOf(i1));
         }
         Collections.shuffle(list);
 
-        log.info("Test started for: " + noHeapStore.getClass());
+        performTest(() -> {
+            for (val x : list) {
+                // Put value
+                noHeapStore.putString(x, x);
+                // Get value
+                // noHeapStore.getString(x);
+            }
+        }, noHeapStore.getClass().getName());
+
+        ConcurrentHashMap<String, String> hashMap = new ConcurrentHashMap<>();
+        performTest(() -> {
+            for (val x : list) {
+                // Put value
+                hashMap.put(x, x);
+                // Get value
+                // hashMap.get(x);
+            }
+        }, hashMap.getClass().getName());
+    }
+
+    // Currently single thread test
+    public static void performTest(FuncToTest funcToTest, String testName) throws InterruptedException {
+        log.info("Thread usage: " + THREAD_POOL_SIZE);
+
+        log.info("Test started for: " + testName);
 
         long averageTime = 0;
-        for (int i = 0; i < 1_000_000; i++) {
+        for (int i = 0; i < 10; i++) {
             long startTime = System.nanoTime();
             ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
             for (int j = 0; j < THREAD_POOL_SIZE; j++) {
-                executorService.execute(() -> {
-                    for (val x : list) {
-                        // Put value
-                        noHeapStore.putString(x, String.valueOf(x));
-
-                        // Get value
-                        noHeapStore.getString(x);
-                    }
-                });
+                executorService.execute(funcToTest::testFunc);
             }
 
             executorService.shutdown();
@@ -63,6 +73,11 @@ public class PerformanceTest {
             log.info("1M entries added/retrieved in " + totalTime + " ms");
         }
 
-        log.info("For " + noHeapStore.getClass() + " the average time is " + averageTime / 10 + " ms\n");
+        log.info("For " + testName + " the average time is " + averageTime / 10 + " ms\n");
+    }
+
+    @FunctionalInterface
+    interface FuncToTest {
+        void testFunc();
     }
 }
